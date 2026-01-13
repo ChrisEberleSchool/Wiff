32af0392758fc7f0d5cbf14fef79d56a1a06dac1
ChrisEberleSchool <156865868+ChrisEberleSchool@users.noreply.github.com>
2026-01-12 21:27:16 -0700

    feat: added simple non-synced loader anim

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <memory>
 #include <vector>
+#include <thread>
+#include <chrono>
 
 #include "command/base/Command.h"
 #include "command/manager/CommandManager.h"
@@ -10,9 +12,22 @@
 #include "command/help/HelpCommand.h"
 #include "argument/ParsedArgs.h"
 
+void loadingAnimation(std::stop_token st) {
+    const char spinner[] = { '|', '/', '-', '\\'};
+    int i = 0;
+
+    while (!st.stop_requested())
+    {
+        std::cout<<"\rLoading "<< spinner[i++ % 4] << std::flush;
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+    
+    std::cout<<"\r              \r"<<std::flush;
+}
 
 int main(int argc, char* argv[]) {
     try {
+        std::jthread loader(loadingAnimation); 
         ParsedArgs args(argc,argv);
         //create the managers and arguments class
         CommandManager cmdManager;
